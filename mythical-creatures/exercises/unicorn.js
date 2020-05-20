17bd9fdf514c7aa3f603c59f54cb79703636c4fc
Rachel Lew <rlew07@gmail.com>
2020-05-20 13:03:47 -0600

    Pass unicorn test

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,7 +1,19 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = 'white') {
     this.name = name;
     this.color = color;
+  }
+
+  isWhite() {
+    if (this.color !== 'white') {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  says(phrase) {
+    return `**;* ${phrase} *;**`
   }
 }
 
